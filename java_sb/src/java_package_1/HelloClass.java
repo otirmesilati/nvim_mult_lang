c1f885f4c2b35088dfd21d044e579e149b018150
Otir <otirmesi@gmail.com>
2024-04-24 13:56:45 +0300

    tweaking

--- java_sb/src/java_package_1/HelloClass.java
+++ java_sb/src/java_package_1/HelloClass.java
@@ -2,9 +2,17 @@ package java_package_1;
 
 public class HelloClass {
 
+  public static void functionOne() {
+
+    System.out.println("1");
+
+  }
+
   public static void main (String args[]){
 
     System.out.println("Hello!");
-  
+
+    functionOne();
+
   }
 }
