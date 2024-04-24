e71a9fd4a1e0664bb0d717240bf6e2f8626d1784
Otir <otirmesi@gmail.com>
2024-04-24 15:30:51 +0300

    Tweaking

--- java_sb/src/java_package_1/HelloClass.java
+++ java_sb/src/java_package_1/HelloClass.java
@@ -4,13 +4,24 @@ public class HelloClass {
 
   public static void functionOne() {
 
-    System.out.println("1");
+    System.out.println("java_package_1");
+    int firstValue = 2;
+    int secondValue = 3;
+    System.out.println(firstValue + secondValue);
+
+  }
+
+public static int functionTwo(){
+
+
+    return 4;
 
   }
 
   public static void main (String args[]){
 
     System.out.println("Hello!");
+    System.out.println(functionTwo());
 
     functionOne();
 
