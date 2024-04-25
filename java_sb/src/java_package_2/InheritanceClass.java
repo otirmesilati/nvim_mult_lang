b3dacf52e1612e4295c84e151d864ef9c3618f3f
Otir <otirmesi@gmail.com>
2024-04-25 11:39:47 +0300

    tweeeeaking

--- java_sb/src/java_package_2/InheritanceClass.java
+++ java_sb/src/java_package_2/InheritanceClass.java
@@ -9,11 +9,22 @@ public class InheritanceClass {
     static String firstString = "b";
   }
 
+
+  public class secondClass {
+
+    static String secondStringFunction(){
+
+      return "This is just for printing";
+
+    }
+
+  }
+  
 	public static void main(String[] args) {
 		
     System.out.println("The three parameters of the first class: \n");
     System.out.println(firstClass.firstInt + ", " + firstClass.firstChar + ", " + firstClass.firstString);
-
+    System.out.println(secondClass.secondStringFunction());
 	}
 
 }
