7b867b58913db191ec1fe50338bec6457546efed
Otir <otirmesi@gmail.com>
2024-04-25 11:02:28 +0300

    tweaking~

--- java_sb/src/java_package_2/InheritanceClass.java
+++ java_sb/src/java_package_2/InheritanceClass.java
@@ -2,6 +2,13 @@ package java_package_2;
 
 public class InheritanceClass {
 
+  public class firstClass {
+
+    int firstInt = 1;
+    char firstChar = 'a';
+    String firstString = "b";
+  }
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
